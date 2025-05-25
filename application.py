34aaf406c559910f64d1cfd8fc8fe82f91b63ec4
miguelenruiz <miguelenruiz1@gmail.com>
2025-05-25 17:08:58 -0500

    archiv

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 from app import create_app, db
-from flask import jsonify, abort
 
 application = create_app()
 app = application  # Add alias for compatibility
@@ -7,13 +6,6 @@ app = application  # Add alias for compatibility
 @application.before_first_request
 def init_app_db():
     db.create_all()
-
-@application.route("/<string:id>")
-def heroe(id):
-    item = data.get(id)
-    if item is None:
-        return jsonify({"error": f"Heroe '{id}' no encontrado"}), 404
-    return jsonify(item)
 
 if __name__ == '__main__':
     #with application.app_context():
