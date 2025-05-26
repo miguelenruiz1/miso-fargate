4d595370285cf837f8dd025663733e7f9c64a452
miguelenruiz <miguelenruiz1@gmail.com>
2025-05-25 22:37:17 -0500

    ajuste de variables

--- config/production.py
+++ config/production.py
@@ -3,6 +3,6 @@ import os
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASSWORD')}"
-        f"@{os.getenv('DATABASE_HOST')}:5432/{os.getenv('DATABASE_NAME')}"
+        f"@{os.getenv('DATABASE_HOST')}:RDS_PORT/{os.getenv('DATABASE_NAME')}"
     )
     DEBUG = False
