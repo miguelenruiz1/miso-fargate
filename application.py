ac3ace8307155727ae6be37cc5a0c1802ba87a35
miguelenruiz <miguelenruiz1@gmail.com>
2025-05-25 20:32:34 -0500

    archivo

--- application.py
+++ application.py
@@ -1,13 +1,16 @@
-from app import create_app, db
+from flask import jsonify
+from app import create_app
 
 application = create_app()
-app = application  # Add alias for compatibility
+app = application  # alias
 
-@application.before_first_request
-def init_app_db():
-    db.create_all()
+@application.route('/ping')
+def ping():
+    return jsonify(status='ok', message='pong'), 200
 
 if __name__ == '__main__':
-    #with application.app_context():
-    #    db.create_all()
+    # Sólo aquí, si quieres, inicializa la BD antes de servir
+    # with application.app_context():
+    #     from app import db
+    #     db.create_all()
     application.run(host='0.0.0.0', port=5000)
